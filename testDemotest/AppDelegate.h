b079ad205be5f59e9c0c08497d815ae1d2c6bdf5
wkcdeie@gmail.com <wkcdeie@gmail.com>
2018-12-03 10:07:55 +0800

    first commit

---
+++ testDemotest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testDemotest
+//
+//  Created by golds on 2018/12/3.
+//  Copyright © 2018年 golds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
